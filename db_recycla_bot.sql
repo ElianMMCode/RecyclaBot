624ada4869f7d0cb2880b245bfd0af9bcf5c4f82
ElianMMCode <elianferney19@gmail.com>
2025-03-31 22:40:59 -0500

    Add files via upload
    Correccion SQL

--- db_recycla_bot.sql
+++ db_recycla_bot.sql
@@ -18,7 +18,7 @@ CREATE DATABASE db_recycla_bot;
 USE db_recycla_bot;
 
 CREATE TABLE tb_people (
-user_id INT PRIMARY KEY AUTO_INCREMENTE,
+people_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(100) NOT NULL,
 last_name VARCHAR(100) NOT NULL,
 email VARCHAR(100) UNIQUE NOT NULL,
@@ -29,69 +29,75 @@ profile_imag VARCHAR (256),
 description TEXT
 );
 
+CREATE TABLE tb_citizens (
+citizen_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+fk_people_id INT NOT NULL
+);
+
+
 CREATE TABLE tb_favorites (
-favorite_id INT PRIMARY KEY AUTO_INCREMENTE,
-fk_user_id INT NOT NULL,
+favorite_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+fk_people_id INT NOT NULL,
 fk_module_id INT NOT NULL
 );
 
 CREATE TABLE tb_modules (
-module_id INT PRIMARY KEY AUTO_INCREMENTE,
+module_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 module_name VARCHAR(100) NOT NULL,
-description TEXT NOT NULL
-fk_notice_id INT
+description TEXT NOT NULL,
+fk_notice_id INT,
 fk_recycling_points_id INT
 );
 
 CREATE TABLE tb_admin (
-admin_id INT PRIMARY KEY AUTO_INCREMENTE,
-fk_user_id INT NOT NULL
+admin_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+fk_people_id INT NOT NULL
 );
 
 CREATE TABLE tb_recyclers (
-recycler_id INT PRIMARY KEY AUTO_INCREMENTE,
-fk_user_id INT NOT NULL
+recycler_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+fk_people_id INT NOT NULL
 );
 
 CREATE TABLE tb_notification (
-notification_id INT PRIMARY KEY AUTO_INCREMENTE,
-fk_user_id INT NOT NULL,
+notification_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+fk_people_id INT NOT NULL,
 message TEXT, NOT NULL,
 fk_module_id INT NOT NULL,
 link VARCHAR(256)
 );
 
 CREATE TABLE tb_chatbot (
-qa_id INT PRIMARY KEY AUTO_INCREMENTE,
+qa_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 question TEXT NOT NULL,
 fk_answer_id INT NOT NULL,
 link VARCHAR(256)
 );
 
 CREATE TABLE tb_answers(
-answer_id INT PRIMARY KEY AUTO_INCREMENTE,
-fk_user_id INT NOT NULL,
+answer_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+fk_people_id INT NOT NULL,
 status VARCHAR(20) NOT NULL,
 comment TEXT
 );
 
 CREATE TABLE tb_comments(
-comment_id INT PRIMARY KEY AUTO_INCREMENTE,
+comment_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 comment TEXT NOT NULL,
 comment_date DATETIME NOT NULL,
-fk_user_id INT NOT NULL,
+fk_people_id INT NOT NULL,
 fk_module_id INT NOT NULL
 );
 
-CREATE TABLE tb_news (
-news_id INT PRIMARY KEY AUTO_INCREMENTE,
+CREATE TABLE tb_publications (
+publication_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 title VARCHAR (130) NOT NULL,
 content TEXT NOT NULL,
 publication_date DATETIME NOT NULL
 );
 
 CREATE TABLE tb_recycling_points (
-point_id INT PRIMARY KEY AUTO_INCREMENTE,
+point_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(100) NOT NULL,
 schedule TEXT NOT NULL,
 fk_recycler_id INT NOT NULL,
@@ -104,31 +110,34 @@ fk_price_table_id INT NOT NULL
 );
 
 CREATE TABLE tb_price_tables (
-price_table_id INT PRIMARY KEY AUTO_INCREMENTE,
+price_table_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 fk_material_id INT NOT NULL,
 price DECIMAL NOT NULL
 );
 
 CREATE TABLE tb_materials (
-material_id INT PRIMARY KEY AUTO_INCREMENTE,
-name VARCHAR(60) NOT NULL
+material_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(60) NOT NULL,
+description TEXT
 );
 
-ALTER TABLE tb_admin ADD FOREIGN KEY (fk_user_id) REFERENCES tb_people (user_id);
-ALTER TABLE tb_recyclers ADD FOREIGN KEY (fk_user_id) REFERENCES tb_people (user_id);
-ALTER TABLE tb_favorites ADD FOREIGN KEY (fk_user_id) REFERENCES tb_people (user_id);
+ALTER TABLE tb_citizens ADD FOREIGN KEY (fk_people_id) REFERENCES tb_people (people_id);
+ALTER TABLE tb_admin ADD FOREIGN KEY (fk_people_id) REFERENCES tb_people (people_id);
+ALTER TABLE tb_recyclers ADD FOREIGN KEY (fk_people_id) REFERENCES tb_people (people_id);
+ALTER TABLE tb_favorites ADD FOREIGN KEY (fk_people_id) REFERENCES tb_people (people_id);
 ALTER TABLE tb_favorites ADD FOREIGN KEY (fk_module_id) REFERENCES tb_modules (module_id);
-ALTER TABLE tb_notification ADD FOREIGN KEY (fk_user_id) REFERENCES tb_people (user_id);
+ALTER TABLE tb_notification ADD FOREIGN KEY (fk_people_id) REFERENCES tb_people (people_id);
 ALTER TABLE tb_notification ADD FOREIGN KEY (fk_module_id) REFERENCES tb_modules (module_id);
 ALTER TABLE tb_chatbot ADD FOREIGN KEY (fk_answer_id) REFERENCES tb_answers (answer_id);
-ALTER TABLE tb_answers ADD FOREIGN KEY (fk_user_id) REFERENCES tb_people (user_id);
-ALTER TABLE tb_comments ADD FOREIGN KEY (fk_user_id) REFERENCES tb_people (user_id);
+ALTER TABLE tb_answers ADD FOREIGN KEY (fk_people_id) REFERENCES tb_people (people_id);
+ALTER TABLE tb_comments ADD FOREIGN KEY (fk_people_id) REFERENCES tb_people (people_id);
 ALTER TABLE tb_comments ADD FOREIGN KEY (fk_module_id) REFERENCES tb_modules (module_id);
 ALTER TABLE tb_recycling_points ADD FOREIGN KEY (fk_recycler_id) REFERENCES tb_recyclers (recycler_id);
 ALTER TABLE tb_recycling_points ADD FOREIGN KEY (fk_price_table_id) REFERENCES tb_price_tables (price_table_id);
 ALTER TABLE tb_price_tables ADD FOREIGN KEY (fk_material_id) REFERENCES tb_materials (material_id);
-ALTER TABLE tb_modules ADD FOREIGN KEY (fk_notice_id) REFERENCES tb_news (news_id);
+ALTER TABLE tb_modules ADD FOREIGN KEY (fk_notice_id) REFERENCES tb_publications (publication_id);
 ALTER TABLE tb_modules ADD FOREIGN KEY (fk_recycling_points_id) REFERENCES tb_recycling_points (point_id);
+
 
 
 
